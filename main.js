3c93ea1b692cc386f53a3ed6668914cfc8c7de03
heraden <127416579+heraden@users.noreply.github.com>
2023-04-23 13:19:37 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const input = prompt("enter") 
-let A = "100" 
-for (i = 1 ; i < 1000; i++){ 
-if(i % input == 0 ) { 
-console.log(i)} 
-}
+document.getElementById("bottom").addEventListener("click", function () { 
+    event.preventDefault(); 
+    let input = document.getElementById("enter").value 
+let A = "100"  
+for (i = 1 ; i < 1000; i++){  
+if(i % input == 0 ) {  
+console.log(i)}  
+} 
+ 
+      })
